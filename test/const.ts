e70302ac33326822019fd6fb2e5b20dd9e2e483c
king-prawns <giorgio.gamberoni@gmail.com>
2022-01-23 01:03:03 +0100

    remove close callback

--- test/const.ts
+++
@@ -1,3 +0,0 @@
-// https://github.com/puppeteer/puppeteer#q-what-features-does-puppeteer-not-support
-export const PATH_TO_CHROME =
-  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
